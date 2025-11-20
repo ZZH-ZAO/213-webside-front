a0bc425096f51d64ac5fddfcc9ac7304868da3b1
ZZH-ZAO <1416686359@qq.com>
2025-11-20 17:38:51 +0800

    封装网络请求

---
+++ my-lab-frontend/src/api/http.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { useAuthStore } from '../stores/auth'
+
+const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:8000',
+  timeout: 10000
+})
+
+http.interceptors.请求.use(cfg => {
+  const auth = useAuthStore()
+  if (auth.token) {
+    cfg.headers = cfg.headers || {}
+    cfg.headers.Authorization = `Bearer ${auth.token}`
+  }
+  return cfg
+})
+
+http.interceptors.response.use(
+  resp => resp.data,
+  err => {
+    // 可以在这里做统一错误处理或弹 Toast
+    return Promise.reject(err)
+  }
+)
+
+export default http
